8dc797a0da332e6b82b15f41146b3721442ba588
Nur <furiousnur@gmail.com>
2023-08-24 23:16:48 +0600

    api add

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,43 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-export default function Blog() {
+async function getData() {
+    let response = await fetch('https://basic-blog.teamrabbil.com/api/post-list/2');
+    let data = await response.json();
+    return data;
+}
+const Page = async () => {
+    let data = await getData();
     return (
         <section className="py-5">
             <div className="container">
-                <h2 className="text-center">Latest Blog Posts</h2>
+                <h2 className="text-center">Blog Posts</h2>
+                <hr/>
+                <br/>
                 <div className="row">
-                    <div className="col-md-4">
-                        <div className="card mb-4">
-                            <img src="https://via.placeholder.com/300" className="card-img-top" alt="Blog Post" />
-                            <div className="card-body">
-                                <h5 className="card-title">Blog Post Title</h5>
-                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                <a href="#" className="btn btn-primary">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card mb-4">
-                            <img src="https://via.placeholder.com/300" className="card-img-top" alt="Blog Post" />
-                            <div className="card-body">
-                                <h5 className="card-title">Blog Post Title</h5>
-                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                <a href="#" className="btn btn-primary">Read More</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-4">
-                        <div className="card mb-4">
-                            <img src="https://via.placeholder.com/300" className="card-img-top" alt="Blog Post" />
-                            <div className="card-body">
-                                <h5 className="card-title">Blog Post Title</h5>
-                                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                <a href="#" className="btn btn-primary">Read More</a>
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        data.map((item, index) => {
+                            return (
+                                <div className="col-md-4" key={index}>
+                                    <div className="card mb-4">
+                                        <img src={item['img']} className="card-img-top" alt="Blog Post" />
+                                        <div className="card-body">
+                                            <h4 className="card-title">{item['title']}</h4>
+                                            <p className="card-text">{item['short']}</p>
+                                            <a href="#" className="btn btn-primary">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
                 </div>
             </div>
         </section>
     );
 }
+export default Page;
